68594c93dc7961e1a84aae6d5753693a06277436
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-06-15 13:31:14 -0300

    Update rutinas.ts

--- pages/api/admin/rutinas.ts
+++ pages/api/admin/rutinas.ts
@@ -30,7 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           series,
           repeticiones,
           peso,
-          ejercicio (
+          ejercicios (
             nombre
           )
         )
