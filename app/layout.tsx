93bd635ef0ef154a78f1510c3713b82005c9a72c
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-08-15 12:40:44 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
+      <body>
           {children}
       </body>
     </html>
