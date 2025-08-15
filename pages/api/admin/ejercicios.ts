5ac49ea5625fd58daf857fe2317bf5c14e424902
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-08-15 18:38:52 -0300

    Update ejercicios.ts

--- pages/api/admin/ejercicios.ts
+++ pages/api/admin/ejercicios.ts
@@ -2,9 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import supabaseAdmin from '@/lib/supabaseAdmin';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { data, error } = await supabaseAdmin
+  const { data, error, count } = await supabaseAdmin
     .from('ejercicios')
-    .select('*'); // trae todos los perfiles
+    .select('*', { count: 'exact' })
+    .order('id', { ascending: true }); // asegura orden
+
+  console.log('💡 Total ejercicios en DB:', count);
 
   if (error) {
     return res.status(500).json({ message: 'Error al traer ejercicios', error });
